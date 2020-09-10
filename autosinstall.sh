e5d34faad385ff1367b8eadf332e03affd331bbe
hao1305430263 <hao1305430263@gmail.com>
2020-09-10 10:10:09 +0800

    lazygit

--- autosinstall.sh
+++ autosinstall.sh
@@ -3,9 +3,21 @@
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 
 sudo apt install curl
+
+# vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+
+# ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
+# lazygit
+sudo add-apt-repository ppa:lazygit-team/release
+sudo apt-get update
+sudo apt-get install lazygit
+
 
 sudo apt-get update
